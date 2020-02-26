e9360a7408c2e0c0ee77171a8ba51622d3d60e8e
David Cid <david.cid@gmail.com>
2020-02-26 23:46:08 +0100

    finished collection-component

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,37 +1,25 @@
 import React from "react";
+import "./Collection.css";
 
 class Collection extends React.Component {
+  hideCollection = () => {
+    console.log("it works!");
+  };
+
   render() {
     return (
-      <ul
-        style={{
-          position: "fixed",
-          bottom: 0,
-          backgroundColor: "#ffffff",
-          width: "100%",
-          zIndex: 99,
-          display: "flex",
-          justifyContent: "flex start",
-          alignItems: "center",
-          flexDirection: "row",
-          flexWrap: "wrap"
-        }}
-      >
-        {this.props.collection.map((item, id) => {
-          return (
-            <li
-              key={id}
-              style={{
-                margin: 7,
-                listStyle: "none"
-              }}
-            >
-              <div className="title">{item.title}</div>
-              <img src={item.thumb} alt={item.title} style={{ width: 95 }} />
-            </li>
-          );
-        })}
-      </ul>
+      <div className="collection-box">
+        <ul className="collection">
+          {this.props.collection.map((item, id) => {
+            return (
+              <li key={id}>
+                <div className="title">{item.title}</div>
+                <img src={item.thumb} alt={item.title} style={{ width: 75 }} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     );
   }
 }
